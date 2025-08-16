3ede124c0b1fa169561b5dfce7e3bf73c19332f9
Vistaar <vistaar007@gmail.com>
2025-08-16 12:29:31 +0530

    Changed sout statement

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World triggered by Git!");
+        System.out.println("New Message!!!");
     }
 }
